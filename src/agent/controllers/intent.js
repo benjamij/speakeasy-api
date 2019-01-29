d55c43913f533df616b49818b9a34adae7042815
Benjamin Jakobus <benjamin.jakobus@gmail.com>
2019-01-29 08:27:59 -0200

    Added error handling

--- src/agent/controllers/intent.js
+++ src/agent/controllers/intent.js
@@ -5,9 +5,8 @@ const router = express.Router();
 router.post('/detect', function (req, res) {
     dialogflow.connect();
     let response = dialogflow.detectIntent(req.query.agent, req.query.text, req.query.language);
-    response.then((data) => {
-        res.send(data);
-    });
+    response.then((data) => res.send(data));
+    response.catch((error) => res.send({'errors': ['Request could not be processed.']}));
 });
 
 module.exports = router
