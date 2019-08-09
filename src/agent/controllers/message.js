b58b6ba2dd500444269263ad362a047b62d7ba5a
Benjamin Jakobus <benjamin.jakobus@gmail.com>
2019-08-09 15:24:02 -0300

    Added POST endpoint
    Will need to be refactored in the future. But will do for now. Allows for creation of new messages.

--- src/agent/controllers/message.js
+++ src/agent/controllers/message.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const passport = require('passport');
+const uuidv4 = require('uuid/v4');
 
 const Message = require('../models/message');
 const Agent = require('../models/agent');
+
+const validators = require('../validators/message');
 
 const router = express.Router();
 
@@ -10,10 +13,26 @@ router.get('/',  passport.authenticate('jwt', {session: false}), async (req, res
     const agentIds = await Agent.findAll({
         raw: true,
         where: {organization_id: req.user.get('organization_id')},
-        attributes: ['id']
+        attributes: ['uuid']
     });
-    const messages = await Message.findAll({where: {id: agentIds.map((item) => {return item.id})}});
+    const config = {
+        where: {agent_uuid: agentIds.map((item) => {return item.uuid})}
+    };
+
+    // TODO: refactor this, since it will be the same across all controllers
+    if (req.query.groupBy) {
+        config['group'] =[req.query.groupBy];
+    }
+    const messages = await Message.findAll(config);
     res.formatter.ok(messages);
+});
+
+router.post('/', validators.post, passport.authenticate('jwt', {session: false}), async (req, res) => {
+    // TODO: refactor this, since it will be the same across all controllers
+    let data = req.body;
+    data.uuid = uuidv4();
+    data.content = JSON.stringify(req.body.content);
+    res.formatter.ok(await Message.create(data, {user: req.user}));
 });
 
 module.exports = router
